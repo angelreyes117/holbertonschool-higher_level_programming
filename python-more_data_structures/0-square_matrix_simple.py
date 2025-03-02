1b6d58faa19315d2c8f4f0b4fad38962c89b84f3
angelreyes117 <angelreyes.117@gmail.com>
2025-03-02 10:52:45 +0000

    more data structures

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+def squared(element):
+    return (element**2)
+
 
 def square_matrix_simple(matrix=[]):
-    """Compute the square value of all integers of a matrix."""
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
+    new = matrix[:]
+    for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                new[i] = list(map(squared, matrix[i]))
+    return new
