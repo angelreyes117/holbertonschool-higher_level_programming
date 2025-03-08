c1934640c98a4e543967f5ecc866eeb8640e56a5
angelreyes117 <angelreyes.117@gmail.com>
2025-03-08 04:12:22 +0000

    inheritance

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
+"""
+    BaseGeometry module
+"""
 
 
-class BaseGeometry:
-    """Reprsent base geometry."""
-
+class BaseGeometry():
+    """
+        BaseGeometry Class
+    """
     def area(self):
-        """Not yet implemented."""
+        """
+            public instance
+            Raise:
+                Exception: Area not implemented
+
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
         """
-        if type(value) != int:
+            Public instance  that validates value
+            Args:
+                name (string): name
+                value(int): Value
+            Raises:
+                TypeError: When Value is not int
+                ValueError: When Value less or equal to 0
+        """
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
