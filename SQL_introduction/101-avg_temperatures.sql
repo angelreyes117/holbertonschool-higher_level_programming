8ef9f0ded6441c53be94786a585646825e70a2e5
angelreyes117 <angelreyes.117@gmail.com>
2025-03-22 02:27:04 +0000

    SQL introduction

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,6 @@
--- Display the average temperature (Fahrenheit) by city from temperatures table, ordered by average temperature descending
-SELECT city, AVG(temperature) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+-- Displays the average temperature (in Fahrenheit)
+-- by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
--- SQL_introduction/103-max_state.sql
+++ SQL_introduction/103-max_state.sql
@@ -1,2 +1,5 @@
--- Display the maximum temperature of each state from temperatures table, ordered by state name
-SELECT state, MAX(temperature) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
+-- Displays the max temperature of each state, ordered by state name.
+SELECT `state`, MAX(`value`) AS `max_temp`
+FROM `temperatures`
+GROUP BY `state`
+ORDER BY `state`;
