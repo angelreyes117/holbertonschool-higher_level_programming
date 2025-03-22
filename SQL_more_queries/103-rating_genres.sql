84c42c4ff4c784994dee4e5a2131f2665d6a6317
angelreyes117 <angelreyes.117@gmail.com>
2025-03-22 03:06:23 +0000

    SQL more queries

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,5 @@
--- Lists all genres by their rating sum
-SELECT tv_genres.name, SUM(tv_shows_rate.rating) AS rating
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows_rate ON tv_show_genres.show_id = tv_shows_rate.show_id
-GROUP BY tv_genres.name
-ORDER BY rating DESC;
+-- Displays the max temperature of each state, ordered by state name.
+SELECT `state`, MAX(`value`) AS `max_temp`
+FROM `temperatures`
+GROUP BY `state`
+ORDER BY `state`;
